fca111cbcf48a1ca33b924654557ca2389ae73c8
krisdw <chenjinwen77@foxmail.com>
2020-01-04 14:34:17 +0800

    rewrote style module rule

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
 'use strict'
-import YButton from './components/y-button'
-const clickHandler = (ev: Event): void => {
-    console.log(ev)
-}
-const yButton = new YButton({
-    parent: document.querySelector('body'),
-    htmlType: 'submit',
-    ariaLabel: 'add',
-    variant: 'contained',
-    color: 'rgba(17, 82, 147, 1)',
-    text: '添加',
-    id: 'y-button',
-    click: clickHandler
-})
