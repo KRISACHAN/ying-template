ff18a6e8303ccf2549e521cd3bbcd6796c73f14b
krisdw <chenjinwen77@foxmail.com>
2020-04-11 01:46:47 +0800

    fixed decorator plugin's bug

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 "use strict";
+class MyClass {
+    constructor() {}
+    output(data) {
+        console.log(data)
+    }
+    send(data) {
+        this.output(data)
+    }
+}
+
+const my = new MyClass()
+my.send('1')
