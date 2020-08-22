f503e8f85c88c68826305a6d122dc714b1255780
krisdw <chenjinwen77@foxmail.com>
2020-08-23 02:22:15 +0800

    feat(line): commit
    添加commit规范工具

--- .cz-config.js
+++ .cz-config.js
@@ -1,21 +1,41 @@
 const czConfig = {
     types: [
-        { value: '特性', name: '特性:    一个新的特性' },
-        { value: '修复', name: '修复:    修复一个Bug' },
-        { value: '文档', name: '文档:    变更的只有文档' },
-        { value: '格式', name: '格式:    空格, 分号等格式修复' },
-        { value: '重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-        { value: '性能', name: '性能:    提升性能' },
-        { value: '测试', name: '测试:    添加一个测试' },
-        { value: '工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-        { value: '回滚', name: '回滚:    代码回退' },
+        { value: 'feat', name: '特性: 新增一个功能' },
+        { value: 'fix', name: '修复: 修复一个Bug' },
+        { value: 'docs', name: '文档: 文档变更' },
+        { value: 'style', name: '格式: 代码格式' },
+        { value: 'refactor', name: '重构: 代码重构' },
+        { value: 'perf', name: '性能: 改善性能' },
+        { value: 'test', name: '测试: 测试代码' },
+        {
+            value: 'build',
+            name:
+                '工具: 变更项目构建或外部依赖（例如scopes: webpack、gulp、npm等）',
+        },
+        {
+            value: 'ci',
+            name:
+                '集成: 更改持续集成软件的配置文件和package中的scripts命令，例如scopes: Travis, Circle等',
+        },
+        {
+            value: 'chore',
+            name: '构建: 变更构建流程或辅助工具',
+        },
+        {
+            value: 'revert',
+            name: '回退: 代码回退',
+        },
     ],
-    // scopes: [
-    //     { name: '模块1' },
-    //     { name: '模块2' },
-    //     { name: '模块3' },
-    //     { name: '模块4' },
-    // ],
+    scopes: [
+        { name: 'test' },
+        { name: 'config' },
+        { name: 'src' },
+        { name: 'style' },
+        { name: 'script' },
+        { name: 'html' },
+        { name: 'readme' },
+        { name: 'line' },
+    ],
     // it needs to match the value for field type. Eg.: 'fix'
     // scopeOverrides: {
     //     fix: [
@@ -30,7 +50,7 @@ const czConfig = {
         type: '选择一种你的提交类型:',
         scope: '选择一个scope (可选):',
         // used if allowCustomScopes is true
-        customScope: 'Denote the SCOPE of this change:',
+        customScope: '选择更改范围:',
         subject: '短说明:\n',
         body: '长说明，使用"|"换行(可选)：\n',
         breaking: '非兼容性说明 (可选):\n',
@@ -39,7 +59,7 @@ const czConfig = {
     },
     allowCustomScopes: true,
     allowBreakingChanges: ['特性', '修复'],
-    // limit subject length
+    // 限制主题长度
     subjectLimit: 100,
 }
 
