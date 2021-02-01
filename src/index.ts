ac67544caf3d538681a185a3c73c1da9943ff691
yujin <yujin@gaoding.com>
2021-02-01 16:43:06 +0800

    fix(test): 修复 jest 无法识别图片文件的问题

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 'use strict'
 import '@/style/index.css'
-import '@/style/index.scss'
 import qrcodeImg from 'static/img/qrcode-all1.png'
 
-const radio = 0.5
-const width = 914
-const height = 439
+window.onload = () => {
+    const radio = 0.25
+    const width = 914
+    const height = 439
 
-console.log(qrcodeImg)
-const qrcode: Element = document.querySelector('.qrcode')
-qrcode.setAttribute('src', qrcodeImg)
+    const qrcode: Element = document.querySelector('.qrcode')
+    qrcode.setAttribute('src', qrcodeImg)
 
-console.log(process.env)
+    console.group('%c快来撩我啊', 'color: #894e54; font-size: 60px;')
+    console.log(
+        '%c+',
+        `
+            font-size: 1px;
+            padding: ${height * radio}px ${width * radio}px;
+            background-image: url(https://fish-pond-1253945200.cos.ap-guangzhou.myqcloud.com/img/base/qrcode-all1.png);
+            background-size: contain;
+            background-repeat: no-repeat;
+            color: transparent;
+        `,
+    )
+    console.groupEnd()
+}
 
-console.group('%c快来撩我啊', 'color: #894e54; font-size: 60px;')
-console.log(
-    '%c+',
-    `
-        font-size: 1px;
-        padding: ${height * radio}px ${width * radio}px;
-        background-image: url(https://fish-pond-1253945200.cos.ap-guangzhou.myqcloud.com/img/base/qrcode-all1.png);
-        background-size: contain;
-        background-repeat: no-repeat;
-        color: transparent;
-    `,
-)
-console.groupEnd()
+export const add = (a: number, b: number): number => a + b
